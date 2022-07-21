c80cc941954867439281203c2ff0070f47c726c0
greeny <hendra_mulyanto@yahoo.com>
2022-07-21 00:37:08 +0000

    modified:   src/GraphicsView.css 	modified:   src/LineGPage.css 	modified:   src/MainPage.css 	modified:   src/MonitorView.css 	modified:   src/MonitorView.tsx 	modified:   src/TokeninPage.css 	new file:   src/images/Swap.png

--- src/MonitorView.tsx
+++ src/MonitorView.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './MonitorView.css';
+import Swap from './images/Swap.png';
+
 
 const MonitorView = () => {
 
 
 return(
 <div className="monitorviewdiv">
-Apa sajalah yang pneting makan
+<div className="swapimagediv">
+<img src={Swap} className="swapimage"></img>
+</div>
+<div className="swapdiv">
+<div className="titleswapdiv">
+<span className="titleswapspan">
+Why DEX Token Price Monitor ?
+</span>
+</div>
+<div className="contentswapdiv">
+<span className="contentswapspan">
+DEX token price monitor brings benefit for users who wants to know if their 
+assets now in profit position. Holding tens to hundreds of different tokens
+will makes taking profit so challenges. Token price monitor can be use to 
+see price movement directly from Dexes too where users token accepted to swap
+on that platform. This makes price realtime and guarantee can be traded. Some
+new breed and profitable tokens which still doesn't have their token name 
+searchable on dexes can be monitored too.
+</span>
+</div>
+</div>
 </div>
 )
 }
